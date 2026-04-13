14c2ae537a40d95545496e88fd255ba4a45b31b0
Keshawn-K <keshawn.kingori@student.moringaschool.com>
2026-04-14 00:01:01 +0300

    added feature to fetch and display random quote

---
+++ quote_vault/index.js
@@ -0,0 +1,9 @@
+async function getQuote() {
+  const response = await fetch("https://dummyjson.com/quotes/random");
+  const data = await response.json();
+
+  document.getElementById("quote").textContent = data.quote;
+  document.getElementById("author").textContent = "- " + data.author;
+}
+
+getQuote();
